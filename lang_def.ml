99244f14e0dcc2dabe8a41b7d243cb43254d19fa
Justin Brick <justin@justinbrick.com>
2021-05-04 22:54:30 -0400

    Expression equivalence and substitution

--- lang_def.ml
+++ lang_def.ml
@@ -41,3 +41,26 @@ let string_of_listing listing =
 
 let string_of_program (pc, listing) =
   Format.sprintf "PC: %d\nListing:\n%s" pc (string_of_listing listing)
+
+(* [x/y]e *)
+let sub_var (x:id) (y:id) (e:expr) =
+  match e with
+    | Var i -> if y = i then Var x else e
+    | Const i -> e
+    | App (e1, e2) -> App (sub_var x y e1, sub_var x y e2)
+    | Lam (z, e1) -> if y = z then Lam (x, sub_var x y e1) else Lam (z, sub_var x y e1)
+    | Add (e1, e2) -> Add (sub_var x y e1, sub_var x y e2)
+    | Sub (e1, e2) -> Sub (sub_var x y e1, sub_var x y e2)
+    | Mul (e1, e2) -> Mul (sub_var x y e1, sub_var x y e2)
+    | Div (e1, e2) -> Div (sub_var x y e1, sub_var x y e2)
+
+let expr_equal e1 e2 =
+  match (e1, e2) with
+    | (Var x, Var y) -> x = y
+    | (Const i, Const j) -> i = j
+    | (App (n1, n2), App (m1, m2)) -> (expr_equal n1 m1) and (expr_equal n2 m2)
+    | (Lam (x, r), Lam (y, t)) -> expr_equal r (sub_var x y t)
+    | (Add (n1, n2), Add (m1, m2)) -> (expr_equal n1 m1) and (expr_equal n2 m2)
+    | (Sub (n1, n2), Sub (m1, m2)) -> (expr_equal n1 m1) and (expr_equal n2 m2)
+    | (Mul (n1, n2), Mul (m1, m2)) -> (expr_equal n1 m1) and (expr_equal n2 m2)
+    | (Div (n1, n2), Div (m1, m2)) -> (expr_equal n1 m1) and (expr_equal n2 m2)
