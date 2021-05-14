410dfc90674c7b31d94a886242e4a8bf0301bff2
Justin Brick <justin@justinbrick.com>
2021-05-14 02:12:25 -0400

    Calculus test case

---
+++ tests/calc.ml
@@ -0,0 +1,7 @@
+let s = fun x -> fun y -> fun z -> x z (y z)
+let k = fun x -> fun y -> x
+let i = fun x -> x
+let i' = i i
+let k' = k k i
+let k'' = s k s k
+let y = fun f -> (fun x -> f (x x)) (fun x -> f (x x))
