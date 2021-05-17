3da7505b27939a1de1e3d29a1123f288f9c4c258
Justin Brick <justin@justinbrick.com>
2021-05-17 13:23:26 -0400

    Core comment to launch.ml

--- launch.ml
+++ launch.ml
@@ -1,3 +1,11 @@
+(* You may also need these if they are not in ~/.ocamlinit *)
+(*
+#use "topfind";;
+#thread;;
+#require "core.top";;
+#require "core.syntax";;
+*)
+
 #load "compiler-libs/ocamlcommon.cma";;
 #use "lang_def.ml";;
 #use "cfg.ml";;
